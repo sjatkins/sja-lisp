75a51bb6b77bcf02bdf5c6f0a3e448dc182d1784
Samantha Atkins <sjatkins@gmail.com>
2022-06-27 01:29:02 -0600

    dict additions

--- dicts.lisp
+++ dicts.lisp
@@ -4,5 +4,8 @@
 (defun dict-values (ht) (alexandria:hash-table-values ht))
 (defun dict-map-keys (fn ht) (alexandria:maphash-keys fn ht))
 (defun dict-map-values (fn ht) (alexandria:maphash-values fn ht))
+(defun dict-get (key ht &optional default)
+  (multiple-value-bind (val present) (gethash key ht)
+    (if present val default)))
 
 
